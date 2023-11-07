35e14c2ef841a210e9b43b2f795e564fcf7cdbd6
onionmccabbage <onionmccabbage@live.com>
2023-11-07 11:28:39 +0000

    using threads with classes

--- principles.py
+++ principles.py
@@ -3,3 +3,11 @@
 
 # In the stateful pattern, we can make EVERYTHING immutable
 # when things change we destroy the old state and create abrand new (immutable) state
+
+# remember - Python is written in C. It is known as cPython
+#there are other implementations of Python:
+# jython - written in Java
+# ironpython - written in .net
+# ipython is written in C (interactive python)
+
+# The GIL (Global Interpreter Lock)
--- using_threads/03_class_thread_b.py
+++ using_threads/03_class_thread_b.py
@@ -2,9 +2,26 @@ from threading import Thread
 import random
 import time
 
+class MyClass(Thread):
+    '''This class inherists from Thread'''
+    def __init__(self, n):
+        Thread.__init__(self) # we must use this (no optional arguments permitted)
+        # super().__init__(self) # calling super always passes a bunch of optional arguments
+        self.n = n # we should validate and mangle
+    def run(self): # in orderr ot be runnable we override the run method of Thread
+        for _ in range(0,10):
+            print(f'{self.n} is sleeping')
+            time.sleep(random.random()*0.1)
+
 def main():
     '''invoke our class as threads'''
-
+    thread_l = []
+    for _ in range(512):
+        thread_l.append(MyClass(_)) # this is a Thread
+    for thread in thread_l:
+        thread.start()
+    for thread in thread_l: # we must start all the threads before joining them
+        thread.join()
 
 if __name__ == '__main__':
     start = time.time()
