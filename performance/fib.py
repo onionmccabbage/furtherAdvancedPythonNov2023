d53392b34181e071b8d86d18f35c08b3261c1a0c
onionmccabbage <onionmccabbage@live.com>
2023-11-08 13:29:47 +0000

    text for review 3

--- performance/fib.py
+++ performance/fib.py
@@ -7,9 +7,8 @@
 # this will generate a profile report
 # we then use Python to read this report
 
-from memory_profiler import profile
-import timeit
-
+# from memory_profiler import profile
+# import timeit
 
 def fib(n):
     '''here is a low performance fibonacci function'''
@@ -24,12 +23,12 @@ def main():
     n=32 # about 1.6sec
     # n=38 # almost 30sec on my laptop
     fib_values_l = []
-    start = timeit.default_timer()
+    # start = timeit.default_timer()
     for _ in range(2, n+1):
         r = fib(_)
         fib_values_l.append(r)
-    end = timeit.default_timer()
-    print(f'Process took {end-start} sec')
+    # end = timeit.default_timer()
+    # print(f'Process took {end-start} sec')
     print(fib_values_l)
 
 if __name__ == '__main__':
