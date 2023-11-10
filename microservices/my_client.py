13a3586d44997f966d1449ab52e3fa0ede699b41
onionmccabbage <onionmccabbage@live.com>
2023-11-10 09:58:26 +0000

    client passing system arguments as message

--- microservices/my_client.py
+++ microservices/my_client.py
@@ -1,3 +1,4 @@
+import sys # we will read the system argument variables
 import socket
 
 def myClient(): # could be a class
@@ -5,7 +6,10 @@ def myClient(): # could be a class
     cli = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     setup_t = ('localhost', 9876) # IP and port (127.0.0.1)
     cli.connect(setup_t)
-    msg = 'hello'
+    if len(sys.argv) > 1: # we ignore member zero (the file name)
+        msg = ', '.join(sys.argv[1:]) # exclude the first member
+    else:
+        msg = 'default'
     cli.send(msg.encode())
     response = cli.recv(1024)
     # we might choose to decode the response
@@ -14,4 +18,5 @@ def myClient(): # could be a class
     
 
 if __name__ == '__main__':
+    print(sys.argv)
     myClient()
