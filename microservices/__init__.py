a2bb8f257bc21886a42048456b0393595f12ca48
onionmccabbage <onionmccabbage@live.com>
2023-11-10 11:27:50 +0000

    lunch day 5

---
+++ microservices/__init__.py
@@ -0,0 +1,5 @@
+# we used to write code that would execute 
+# when ANY module in this package was run
+# it was used as a means to configure the running modules
+# For a time, an empty __init__ was required to make a package
+# It is no longer required
